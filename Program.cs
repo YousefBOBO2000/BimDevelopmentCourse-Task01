8b8102aa6c5f52ef88a50465343c04196c49a7d5
Yossf <Yossf@BOBO>
2024-05-11 22:14:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Variable
+            #region HideRegion
+            int Variable01 = 5;
+            int Variable02 = 4;
+            int Variable03 = 6;
+
+            float numFloat = 0.95f;
+            decimal Denum = 1.12m;
+            double Dubnum = 2.24;
+
+            char MyGrade = 'a';
+            Console.WriteLine(MyGrade);
+            #endregion
+
+            bool isCharpFun = false;
+            bool isFishTasty = true;
+            Console.WriteLine("Is Charp Fun??");
+            Console.WriteLine(isCharpFun);
+            Console.WriteLine("Is Fish Tasty??");
+            Console.WriteLine(isFishTasty);
+            if (isCharpFun == true)
+            {
+                Console.WriteLine("Good Luck For You");
+            }
+            else
+            {
+                Console.WriteLine("Good For You");
+            }
+            int Num01 = 3;
+            int Num02 = 4;
+            double Num03 = 2.6;
+            Num01 = Num02 + (int)Num03;
+            Console.WriteLine(Num01);
+            int Y = 3;
+            bool result3 = (Y < 3 && Y < 5);
+            Console.WriteLine(result3);
+            string text = "Hello Word";
+            print(text);
+
+        }
+        static void print(string text)
+        {
+
+            Console.WriteLine(text);
+        }
+           
+
+	
+
+	}
+    }
+
---
+++ Session2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region Variables
+            //Console.WriteLine("What's the Room Width");
+            //Double Width = Double.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //Console.WriteLine("What's the Room Height");
+            //Double Height = Double.Parse(Console.ReadLine());
+            //Console.WriteLine("What's the Room Length");
+            //double Length = double.Parse(Console.ReadLine());
+            //#endregion
+            //#region STR.
+            //Console.WriteLine("What's the Beam Length");
+            //double BeamLength = double.Parse(Console.ReadLine());
+            //Console.WriteLine("What's the Beam Weight Per meter");
+            //double Weight = double.Parse(Console.ReadLine());
+            //Console.WriteLine(BeamLength * Weight);
+            //#endregion
+            #region DataTypes OF Arch.
+            bool availability = true;
+            if (availability == true)
+            {
+                Console.WriteLine("available For disabled People");
+            }
+            else
+            {
+                Console.WriteLine("no available");
+            } 
+            #endregion
+          
+
+                Console.WriteLine("The cost Per Unit Area:");
+                double CostPerUnitArea =double.Parse(Console.ReadLine());
+            Console.WriteLine(  " TotalArea = ");
+            double TotalArea = double.Parse(Console.ReadLine());
+            double AreaCost = TotalArea*CostPerUnitArea;
+            Console.WriteLine(AreaCost);
+
+        }
+    }
+}
